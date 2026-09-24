3c4299cf8cd750bfe9639feac2d1f7fe9c83b4a0
LIONEIDA MACHADO BARBOSA <64732134068@senacrs.edu.br>
2026-09-23 21:46:18 -0300

    exercicios

---
+++ exercicio/mago.py
@@ -0,0 +1,21 @@
+class mago:
+    pontos_de_vida:int
+pontos_de_magia:int
+capacidade:int=50
+
+def __init__(self,ponto_pontos_de_vida:int,pontos_de_magia:int):
+   
+   self.pontos_de_vida=ponto_pontos_de_vida
+   self.pontos_de_magia=pontos_de_magia
+
+#instancia
+
+mago=mago(30,50)#ou
+mago_vekna=mago(pontos_de_magia=50,_pontos_de_vida=30)
+ 
+
+
+
+    
+    
+
